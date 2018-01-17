4315c46acf06780d21fca2efdf1384b4ee0bdf26
mistery-li <litujin1123@gmail.com>
2018-01-17 22:50:42 +0800

    自制编程语言学习

---
+++ cmake-build-debug/CMakeFiles/crowbar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crowbar.dir/main.c.obj"
+  "crowbar.pdb"
+  "crowbar.exe"
+  "crowbar.exe.manifest"
+  "libcrowbar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/crowbar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/crowbar.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for crowbar.
+# This may be replaced when dependencies are built.
